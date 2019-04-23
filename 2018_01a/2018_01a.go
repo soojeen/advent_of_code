1b41dda266343d01e2233d6e24f4d2be4f37a71a
soojeen <soojeen@gmail.com>
2019-04-22 22:55:38 -0400

    01a

--- 2018_01a/2018_01a.go
+++ 2018_01a/2018_01a.go
@@ -1,15 +1,51 @@
 package main
 
 import "fmt"
+import "log"
+import "strconv"
+import "strings"
 import "advent_of_code/utils"
 
 func main() {
-
-  input, err := utils.ReadInput("input.txt")
-
+  rawInput, err := utils.ReadInput("rawInput.txt")
   if err != nil {
-    fmt.Println("File Read Error")
+    log.Fatal(err)
   }
 
-  fmt.Println(input)
+  input, err := parseInput(rawInput)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  result := sumChanges(input)
+
+  fmt.Println(result)
+}
+
+func parseInput(input string) ([]int, error) {
+  changes := strings.Split(input, "\n")
+
+  new := make([]int, len(changes))
+  var err error
+
+  for i, changeStr := range changes {
+    changeInt, e := strconv.Atoi(changeStr)
+    if e != nil {
+      err = e
+      break
+    }
+
+    new[i] = changeInt
+  }
+
+  return new, err
+}
+
+func sumChanges(changes []int) int {
+  sum := 0
+  for _, change := range changes {
+    sum += change
+  }
+
+  return sum
 }
