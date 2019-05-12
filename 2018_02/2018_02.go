da45a7e354506abef3fcb7a7291031c54d5e3caf
soojeen <soojeen@gmail.com>
2019-05-11 23:20:39 -0400

    go 2a

--- 2018_02/2018_02.go
+++ 2018_02/2018_02.go
@@ -13,29 +13,67 @@ func main() {
 
 	input := strings.Split(rawInput, "\n")
 
-	resultA := checksum(input)
+	resultA := checksumGo(input)
 
 	fmt.Println("a:", resultA)
 	// fmt.Println("b:", resultB)
 }
 
-func checksum(input []string) int {
-	// TODO: swap to concurrent go routines and use channels
+// func checksum(input []string) int {
+// 	// TODO: swap to concurrent go routines and use channels
+// 	twos := 0
+// 	threes := 0
+// 	for _, id := range input {
+// 		charCounts := uniqueCharCount(id)
+
+// 		if charCountHasCount(charCounts, 2) {
+// 			twos += 1
+// 		}
+
+// 		if charCountHasCount(charCounts, 3) {
+// 			threes += 1
+// 		}
+// 	}
+
+// 	return twos * threes
+// }
+
+func checksumGo(input []string) int {
+	type TwoThree struct {
+		two   bool
+		three bool
+	}
+
+	inputCount := len(input)
+	c := make(chan TwoThree, inputCount)
+
+	for _, id := range input {
+		id := id
+		go func() {
+			charCounts := uniqueCharCount(id)
+			two := charCountHasCount(charCounts, 2)
+			three := charCountHasCount(charCounts, 3)
+
+			c <- TwoThree{two, three}
+		}()
+	}
+
 	twos := 0
 	threes := 0
-	for _, id := range input {
-		charCounts := uniqueCharCount(id)
-
-		if charCountHasCount(charCounts, 2) {
-			twos += 1
-		}
-
-		if charCountHasCount(charCounts, 3) {
-			threes += 1
-		}
+	for i := 0; i < inputCount; i++ {
+		twoThree := <-c
+		twos += boolToCount(twoThree.two)
+		threes += boolToCount(twoThree.three)
 	}
 
 	return twos * threes
+}
+
+func boolToCount(booly bool) int {
+	if booly {
+		return 1
+	}
+	return 0
 }
 
 func charCountHasCount(charCounts map[string]int, count int) bool {
