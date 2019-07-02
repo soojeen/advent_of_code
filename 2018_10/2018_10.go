23753748dea0f052821ac95f70c963dc01115f74
soojeen <soojeen@gmail.com>
2019-07-01 20:26:05 -0400

    working a and b

--- 2018_10/2018_10.go
+++ 2018_10/2018_10.go
@@ -1,9 +1,12 @@
 package main
 
+import "bufio"
 import "fmt"
 import "log"
-
-// import "strconv"
+import "os"
+import "regexp"
+import "sort"
+import "strconv"
 import "strings"
 import "advent_of_code/utils"
 
@@ -15,11 +18,7 @@ func main() {
 
 	input := parseInput(rawInput)
 
-	// resultA := winningScore(input, 1)
-	// resultB := winningScore(input, 100)
-
-	fmt.Println("a:", input)
-	// fmt.Println("b:", resultB)
+	mainLights(input)
 }
 
 type light struct {
@@ -27,14 +26,101 @@ type light struct {
 	velocity [2]int
 }
 
-func parseInput(rawInput string) []light {
-	lines := strings.Split(rawInput, "\n")
-	lights := make([]light, len(lines))
+type lights struct {
+	lights     []light
+	positionsX []int
+	positionsY []int
+}
 
-	for _, line := range lines {
-		fmt.Println("line:", line)
+func (l *lights) move() {
+	var positionsX []int
+	var positionsY []int
 
+	for i := range l.lights {
+		l.lights[i].position[0] += l.lights[i].velocity[0]
+		l.lights[i].position[1] += l.lights[i].velocity[1]
+
+		positionsX = append(positionsX, l.lights[i].position[0])
+		positionsY = append(positionsY, l.lights[i].position[1])
 	}
 
-	return lights
+	sort.Ints(positionsX)
+	sort.Ints(positionsY)
+
+	l.positionsX = positionsX
+	l.positionsY = positionsY
+}
+
+func (l *lights) draw() {
+	const margin = 10
+	const inputLen = 391
+
+	mapping := [inputLen][inputLen]string{}
+
+	for y, row := range mapping {
+		for x := range row {
+			mapping[y][x] = "."
+		}
+	}
+
+	for _, light := range l.lights {
+		mapping[light.position[1]][light.position[0]] = "#"
+	}
+
+	for _, row := range mapping {
+		fmt.Println(strings.Join(row[:], ""))
+	}
+}
+
+func parseInput(rawInput string) lights {
+	r, _ := regexp.Compile("position=<(.{6}), (.{6})> velocity=<(.{2}), (.{2})>")
+	lines := strings.Split(rawInput, "\n")
+
+	lightsList := make([]light, len(lines))
+	var positionsX []int
+	var positionsY []int
+
+	for i, line := range lines {
+		match := r.FindStringSubmatch(line)
+
+		positionX, _ := strconv.Atoi(strings.TrimSpace(match[1]))
+		positionY, _ := strconv.Atoi(strings.TrimSpace(match[2]))
+		velocityX, _ := strconv.Atoi(strings.TrimSpace(match[3]))
+		velocityY, _ := strconv.Atoi(strings.TrimSpace(match[4]))
+
+		lightsList[i] = light{[2]int{positionX, positionY}, [2]int{velocityX, velocityY}}
+
+		positionsX = append(positionsX, positionX)
+		positionsY = append(positionsY, positionY)
+	}
+
+	sort.Ints(positionsX)
+	sort.Ints(positionsY)
+
+	return lights{lightsList, positionsX, positionsY}
+}
+
+func mainLights(lights lights) {
+	const diffThreshold = 100
+	lightsLen := len(lights.lights)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for i := 0; ; i++ {
+		xDiff := lights.positionsX[0] - lights.positionsX[lightsLen-1]
+		xDiffStop := (xDiff < 0 && xDiff > -diffThreshold) || (xDiff > 0 && xDiff < diffThreshold)
+
+		yDiff := lights.positionsY[0] - lights.positionsY[lightsLen-1]
+		yDiffStop := (yDiff < 0 && yDiff > -diffThreshold) || (yDiff > 0 && yDiff < diffThreshold)
+
+		if xDiffStop && yDiffStop {
+			for j := i; ; j++ {
+				lights.draw()
+				fmt.Println("second: ", j)
+				scanner.Scan()
+				lights.move()
+			}
+		}
+
+		lights.move()
+	}
 }
