a8318ddfda732a47a91c2870b2bf6587e83b846f
soojeen <soojeen@gmail.com>
2019-07-01 21:26:58 -0400

    initial 11

---
+++ 2018_11/2018_11.go
@@ -0,0 +1,18 @@
+package main
+
+// import "bufio"
+import "fmt"
+
+// import "log"
+// import "os"
+// import "regexp"
+// import "strconv"
+// import "strings"
+// import "advent_of_code/utils"
+
+func main() {
+	input := 1955
+	fmt.Println(input)
+
+	// mainLights(input)
+}
