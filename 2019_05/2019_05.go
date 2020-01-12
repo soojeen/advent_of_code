82c0e286a74f2ad1ab6700d865431b571f49f2c8
soojeen <soojeen@gmail.com>
2020-01-11 21:04:55 -0500

    huge refactor

--- 2019_05/2019_05.go
+++ 2019_05/2019_05.go
@@ -14,8 +14,8 @@ func main() {
 
 	program := parseInput(rawInput)
 
-	// resultA := process(program, 1)
-	// fmt.Println(resultA)
+	resultA := process(program, 1)
+	fmt.Println(resultA)
 
 	resultB := process(program, 5)
 	fmt.Println(resultB)
@@ -45,199 +45,90 @@ type computer struct {
 	output          int
 }
 
-type instruction struct {
-	opCode        int
-	rawOpCode     int
-	rawParameters []int
+func (c *computer) getRawParameter(index int) int {
+	return c.program[c.currentPosition+index+1]
 }
 
-func (i *instruction) getParameter(position int) (int, int) {
-	parameter := i.rawParameters[position]
+func (c *computer) getParameter(index int) int {
+	value := c.getRawParameter(index)
+	rawOpCode := c.program[c.currentPosition]
 	mode := 0
 
-	switch position {
+	switch index {
 	case 0:
-		mode = (i.rawOpCode % 1000) / 100
-		return parameter, mode
+		mode = (rawOpCode % 1000) / 100
 	case 1:
-		mode = (i.rawOpCode % 10000) / 1000
-		return parameter, mode
-	case 2:
-		return parameter, mode
-	default:
-		return 0, 0
+		mode = (rawOpCode % 10000) / 1000
 	}
+
+	if mode == 0 {
+		return c.program[value]
+	}
+
+	return value
 }
 
-func (c *computer) parseInstruction() instruction {
+func (c *computer) processInstruction() {
 	rawOpCode := c.program[c.currentPosition]
 	opCode := rawOpCode % 100
+	_program := c.program
 
 	switch opCode {
 	case 1:
-		fallthrough
-	case 2:
-		fallthrough
-	case 7:
-		fallthrough
-	case 8:
-		rawParameters := make([]int, 3)
-		for i := range rawParameters {
-			rawParameters[i] = c.program[c.currentPosition+i+1]
-		}
-		return instruction{opCode, rawOpCode, rawParameters}
-	case 3:
-		fallthrough
-	case 4:
-		rawParameters := []int{c.program[c.currentPosition+1]}
-		return instruction{opCode, rawOpCode, rawParameters}
-	case 5:
-		fallthrough
-	case 6:
-		rawParameters := make([]int, 2)
-		for i := range rawParameters {
-			rawParameters[i] = c.program[c.currentPosition+i+1]
-		}
-		return instruction{opCode, rawOpCode, rawParameters}
-	case 99:
-		fallthrough
-	default:
-		return instruction{opCode, rawOpCode, []int{}}
-	}
-}
-
-func (c *computer) processInstruction(instruction instruction) {
-	_program := c.program
-
-	switch instruction.opCode {
-	case 1:
-		augend, augendMode := instruction.getParameter(0)
-		if augendMode == 0 {
-			augend = _program[augend]
-		}
-
-		addend, addendMode := instruction.getParameter(1)
-		if addendMode == 0 {
-			addend = _program[addend]
-		}
-
-		targetParameter, _ := instruction.getParameter(2)
-
-		_program[targetParameter] = augend + addend
+		_program[c.getRawParameter(2)] = c.getParameter(0) + c.getParameter(1)
 		c.currentPosition += 4
 	case 2:
-		multiplicand, multiplicandMode := instruction.getParameter(0)
-		if multiplicandMode == 0 {
-			multiplicand = _program[multiplicand]
-		}
-
-		multiplier, multiplierMode := instruction.getParameter(1)
-		if multiplierMode == 0 {
-			multiplier = _program[multiplier]
-		}
-
-		targetParameter, _ := instruction.getParameter(2)
-
-		_program[targetParameter] = multiplicand * multiplier
+		_program[c.getRawParameter(2)] = c.getParameter(0) * c.getParameter(1)
 		c.currentPosition += 4
 	case 3:
-		targetParameter, _ := instruction.getParameter(0)
-
-		_program[targetParameter] = c.input
+		_program[c.getRawParameter(0)] = c.input
 		c.currentPosition += 2
 	case 4:
-		value, mode := instruction.getParameter(0)
-		if mode == 0 {
-			value = _program[value]
-		}
-
-		c.output = value
+		c.output = c.getParameter(0)
 		c.currentPosition += 2
 	case 5:
-		value, mode := instruction.getParameter(0)
-		if mode == 0 {
-			value = _program[value]
-		}
-
-		if value != 0 {
-			instruction, instructionMode := instruction.getParameter(1)
-			if instructionMode == 0 {
-				instruction = _program[instruction]
-			}
-
-			_program[c.currentPosition] = instruction
+		if c.getParameter(0) != 0 {
+			c.currentPosition = c.getParameter(1)
 		} else {
 			c.currentPosition += 3
 		}
 	case 6:
-		value, mode := instruction.getParameter(0)
-		if mode == 0 {
-			value = _program[value]
-		}
-
-		if value == 0 {
-			instruction, instructionMode := instruction.getParameter(1)
-			if instructionMode == 0 {
-				instruction = _program[instruction]
-			}
-
-			_program[c.currentPosition] = instruction
+		if c.getParameter(0) == 0 {
+			c.currentPosition = c.getParameter(1)
 		} else {
 			c.currentPosition += 3
 		}
 	case 7:
-		operandA, operandAMode := instruction.getParameter(0)
-		if operandAMode == 0 {
-			operandA = _program[operandA]
+		value := 0
+		if c.getParameter(0) < c.getParameter(1) {
+			value = 1
 		}
 
-		operandB, operandBMode := instruction.getParameter(1)
-		if operandBMode == 0 {
-			operandB = _program[operandB]
-		}
-
-		targetParameter, _ := instruction.getParameter(2)
-		if operandA < operandB {
-			_program[targetParameter] = 1
-		} else {
-			_program[targetParameter] = 0
-		}
+		_program[c.getRawParameter(2)] = value
+		c.currentPosition += 4
 	case 8:
-		operandA, operandAMode := instruction.getParameter(0)
-		if operandAMode == 0 {
-			operandA = _program[operandA]
+		value := 0
+		if c.getParameter(0) == c.getParameter(1) {
+			value = 1
 		}
 
-		operandB, operandBMode := instruction.getParameter(1)
-		if operandBMode == 0 {
-			operandB = _program[operandB]
-		}
-
-		targetParameter, _ := instruction.getParameter(2)
-		if operandA == operandB {
-			_program[targetParameter] = 1
-		} else {
-			_program[targetParameter] = 0
-		}
+		_program[c.getRawParameter(2)] = value
+		c.currentPosition += 4
 	case 99:
-		c.halt = true
+		fallthrough
 	default:
 		c.halt = true
 	}
-
-	c.program = _program
 
 	return
 }
 
 func process(program []int, input int) int {
-	computer := computer{program, 0, false, input, 0}
+	programCopy := append(program[:0:0], program...)
+	computer := computer{programCopy, 0, false, input, 0}
 
 	for {
-		instruction := computer.parseInstruction()
-		computer.processInstruction(instruction)
-
-		fmt.Println(instruction, computer.output)
+		computer.processInstruction()
 
 		if computer.halt {
 			break
