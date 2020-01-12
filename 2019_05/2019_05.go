7a6bcc1f1929cca0e77d14b26dbacd2dda3d84ae
soojeen <soojeen@gmail.com>
2020-01-11 21:50:49 -0500

    intcomp module

--- 2019_05/2019_05.go
+++ 2019_05/2019_05.go
@@ -5,6 +5,7 @@ import "log"
 import "strconv"
 import "strings"
 import "advent_of_code/utils"
+import "advent_of_code/intcomp"
 
 func main() {
 	rawInput, err := utils.ReadInput("input.txt")
@@ -37,124 +38,12 @@ func parseInput(rawInput string) []int {
 	return positions
 }
 
-type computer struct {
-	program            []int
-	instructionPointer int
-	halt               bool
-	input              int
-	output             int
-}
-
-func (c *computer) getCurrentOpCode() int {
-	rawOpCode := c.program[c.instructionPointer]
-	return rawOpCode % 100
-}
-
-func (c *computer) getRawParameter(index int) int {
-	return c.program[c.instructionPointer+index+1]
-}
-
-func (c *computer) getParameter(index int) int {
-	value := c.getRawParameter(index)
-	rawOpCode := c.program[c.instructionPointer]
-	mode := 0
-
-	switch index {
-	case 0:
-		mode = (rawOpCode % 1000) / 100
-	case 1:
-		mode = (rawOpCode % 10000) / 1000
-	}
-
-	if mode == 0 {
-		return c.program[value]
-	}
-
-	return value
-}
-
-func (c *computer) logic() {
-	opCode := c.getCurrentOpCode()
-
-	switch opCode {
-	case 1:
-		c.program[c.getRawParameter(2)] = c.getParameter(0) + c.getParameter(1)
-	case 2:
-		c.program[c.getRawParameter(2)] = c.getParameter(0) * c.getParameter(1)
-	case 3:
-		c.program[c.getRawParameter(0)] = c.input
-	case 4:
-		c.output = c.getParameter(0)
-	case 5:
-	case 6:
-	case 7:
-		value := 0
-		if c.getParameter(0) < c.getParameter(1) {
-			value = 1
-		}
-
-		c.program[c.getRawParameter(2)] = value
-	case 8:
-		value := 0
-		if c.getParameter(0) == c.getParameter(1) {
-			value = 1
-		}
-
-		c.program[c.getRawParameter(2)] = value
-	case 99:
-		fallthrough
-	default:
-		c.halt = true
-	}
-}
-
-func (c *computer) movePointer() {
-	opCode := c.getCurrentOpCode()
-
-	switch opCode {
-	case 1:
-		fallthrough
-	case 2:
-		fallthrough
-	case 7:
-		fallthrough
-	case 8:
-		c.instructionPointer += 4
-	case 3:
-		fallthrough
-	case 4:
-		c.instructionPointer += 2
-	case 5:
-		if c.getParameter(0) != 0 {
-			c.instructionPointer = c.getParameter(1)
-		} else {
-			c.instructionPointer += 3
-		}
-	case 6:
-		if c.getParameter(0) == 0 {
-			c.instructionPointer = c.getParameter(1)
-		} else {
-			c.instructionPointer += 3
-		}
-	}
-}
-
-func (c *computer) processInstruction() {
-	c.logic()
-	c.movePointer()
-}
-
 func process(program []int, input int) int {
 	programCopy := append(program[:0:0], program...)
-	computer := computer{programCopy, 0, false, input, 0}
-
-	for {
-		computer.processInstruction()
-
-		if computer.halt {
-			break
-		}
+	computer := intcomp.Computer{
+		Program: programCopy,
+		Input:   input,
 	}
 
-	return computer.output
+	return computer.RunProgram()
 }
---
+++ intcomp/intcomp.go
@@ -0,0 +1,121 @@
+package intcomp
+
+// Computer - advent of code Int Comp
+type Computer struct {
+	Program            []int
+	instructionPointer int
+	halt               bool
+	Input              int
+	output             int
+}
+
+func (c *Computer) getCurrentOpCode() int {
+	rawOpCode := c.Program[c.instructionPointer]
+	return rawOpCode % 100
+}
+
+func (c *Computer) getRawParameter(index int) int {
+	return c.Program[c.instructionPointer+index+1]
+}
+
+func (c *Computer) getParameter(index int) int {
+	value := c.getRawParameter(index)
+	rawOpCode := c.Program[c.instructionPointer]
+	mode := 0
+
+	switch index {
+	case 0:
+		mode = (rawOpCode % 1000) / 100
+	case 1:
+		mode = (rawOpCode % 10000) / 1000
+	}
+
+	if mode == 0 {
+		return c.Program[value]
+	}
+
+	return value
+}
+
+func (c *Computer) logic() {
+	opCode := c.getCurrentOpCode()
+
+	switch opCode {
+	case 1:
+		c.Program[c.getRawParameter(2)] = c.getParameter(0) + c.getParameter(1)
+	case 2:
+		c.Program[c.getRawParameter(2)] = c.getParameter(0) * c.getParameter(1)
+	case 3:
+		c.Program[c.getRawParameter(0)] = c.Input
+	case 4:
+		c.output = c.getParameter(0)
+	case 5:
+	case 6:
+	case 7:
+		value := 0
+		if c.getParameter(0) < c.getParameter(1) {
+			value = 1
+		}
+
+		c.Program[c.getRawParameter(2)] = value
+	case 8:
+		value := 0
+		if c.getParameter(0) == c.getParameter(1) {
+			value = 1
+		}
+
+		c.Program[c.getRawParameter(2)] = value
+	case 99:
+		fallthrough
+	default:
+		c.halt = true
+	}
+}
+
+func (c *Computer) movePointer() {
+	opCode := c.getCurrentOpCode()
+
+	switch opCode {
+	case 1:
+		fallthrough
+	case 2:
+		fallthrough
+	case 7:
+		fallthrough
+	case 8:
+		c.instructionPointer += 4
+	case 3:
+		fallthrough
+	case 4:
+		c.instructionPointer += 2
+	case 5:
+		if c.getParameter(0) != 0 {
+			c.instructionPointer = c.getParameter(1)
+		} else {
+			c.instructionPointer += 3
+		}
+	case 6:
+		if c.getParameter(0) == 0 {
+			c.instructionPointer = c.getParameter(1)
+		} else {
+			c.instructionPointer += 3
+		}
+	}
+}
+
+// RunProgram - run program on Int Comp
+func (c *Computer) RunProgram() int {
+	c.halt = false
+	c.instructionPointer = 0
+
+	for {
+		c.logic()
+		c.movePointer()
+
+		if c.halt {
+			break
+		}
+	}
+
+	return c.output
+}
