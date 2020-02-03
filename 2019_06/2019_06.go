bb4bb7d6f20a125b10e61db562ae8785f0afd839
soojeen <soojeen@gmail.com>
2020-02-03 00:12:32 -0500

    build nodes

--- 2019_06/2019_06.go
+++ 2019_06/2019_06.go
@@ -5,31 +5,61 @@ import "log"
 import "strings"
 import "advent_of_code/utils"
 
+type node struct {
+	value    string
+	children []*node
+}
+
 func main() {
 	rawInput, err := utils.ReadInput("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	orbits := parseInput(rawInput)
+	root := parseInput(rawInput)
 
-	resultA := process(orbits)
+	resultA := process(root)
 	fmt.Println(resultA)
 }
 
-func parseInput(rawInput string) [][]string {
+func parseInput(rawInput string) *node {
+	lookup := make(map[string]*node)
+	// to find root
+	// parentLookup := make(map[string]*node)
+
 	orbitPairs := strings.Split(rawInput, "\n")
 
-	orbits := make([][]string, len(orbitPairs))
-	for i := range orbits {
-		orbitPair := strings.Split(orbitPairs[i], ")")
+	for _, orbitPair := range orbitPairs {
+		orbitPair := strings.Split(orbitPair, ")")
 
-		orbits[i] = orbitPair
+		valueB := orbitPair[1]
+		nodeB := lookup[valueB]
+
+		if nodeB == nil {
+			nodeB = &node{valueB, []*node{}}
+		}
+
+		valueA := orbitPair[0]
+		nodeA := lookup[valueA]
+
+		if nodeA == nil {
+			nodeA = &node{valueA, []*node{nodeB}}
+		} else {
+			nodeA.children = append(nodeA.children, nodeB)
+		}
+
+		lookup[valueA] = nodeA
+		lookup[valueB] = nodeB
 	}
 
-	return orbits
+	fmt.Println(lookup["ZZZ"])
+	fmt.Println(lookup["ZZZ"].children[0])
+
+	rootNode := node{"a", make([]*node, 0)}
+
+	return &rootNode
 }
 
-func process(orbits [][]string) int {
+func process(root *node) int {
 	return 1
 }
