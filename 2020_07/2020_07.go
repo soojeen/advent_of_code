fbe31b7f1f1a9da27ed8c8f1859629ff8031e6c8
soojeen <soojeen@gmail.com>
2020-12-07 23:27:22 -0500

    track children count

--- 2020_07/2020_07.go
+++ 2020_07/2020_07.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 import "log"
 import "regexp"
+import "strconv"
 import "strings"
 import "advent_of_code/utils"
 
+type childBag struct {
+	color string
+	count int
+}
+
 type luggageRule struct {
 	container string
-	children  []string
+	children  []childBag
 }
+
+type tracker map[string]bool
 
 func main() {
 	rawInput, readError := utils.ReadInput("input.txt")
@@ -17,8 +25,10 @@ func main() {
 		log.Fatal(readError)
 	}
 
-	input := parseInput(rawInput)
-
+	input, parseError := parseInput(rawInput)
+	if parseError != nil {
+		log.Fatal(parseError)
+	}
 	resultA := countParents(input, "shiny gold")
 	// resultB := countAll(input, countGroupB)
 
@@ -26,40 +36,50 @@ func main() {
 	// fmt.Println("b:", resultB)
 }
 
-func parseInput(input string) []luggageRule {
+func parseInput(input string) ([]luggageRule, error) {
+	var err error
 	rules := strings.Split(input, "\n")
 	luggageRules := make([]luggageRule, len(rules))
 	containerRe := regexp.MustCompile(`(\w+ \w+) bags contain`)
-	childrenRe := regexp.MustCompile(` \d (\w+ \w+) bags?[,.]`)
+	childrenRe := regexp.MustCompile(` (\d) (\w+ \w+) bags?[,.]`)
 
 	for i, rule := range rules {
 		containerMatch := containerRe.FindStringSubmatch(rule)
 		childrenMatch := childrenRe.FindAllStringSubmatch(rule, -1)
 
-		children := make([]string, len(childrenMatch))
+		children := make([]childBag, len(childrenMatch))
 		for i, childMatch := range childrenMatch {
 			if childMatch == nil {
 				continue
 			}
 
-			children[i] = childMatch[1]
+			count, error := strconv.Atoi(childMatch[1])
+			if error != nil {
+				err = error
+				break
+			}
+			children[i] = childBag{childMatch[2], count}
+		}
+
+		if err != nil {
+			break
 		}
 
 		luggageRules[i] = luggageRule{containerMatch[1], children}
 	}
 
-	return luggageRules
+	return luggageRules, err
 }
 
 func countParents(input []luggageRule, color string) int {
-	childToParents := make(map[string]map[string]bool)
+	childToParents := make(map[string]tracker)
 	for _, rule := range input {
 		for _, child := range rule.children {
-			if childToParents[child] == nil {
-				childToParents[child] = map[string]bool{rule.container: true}
+			if childToParents[child.color] == nil {
+				childToParents[child.color] = tracker{rule.container: true}
 			}
 
-			childToParents[child][rule.container] = true
+			childToParents[child.color][rule.container] = true
 		}
 	}
 
@@ -68,25 +88,23 @@ func countParents(input []luggageRule, color string) int {
 	return len(parents)
 }
 
-func getParents(input map[string]map[string]bool, color string) map[string]bool {
+func getParents(input map[string]tracker, color string) tracker {
 	parents := input[color]
+	moreParents := make(tracker)
 
 	if parents == nil || len(parents) == 0 {
-		input[color] = nil
-		return make(map[string]bool)
+		return moreParents
 	}
 
-	moreParents := make(map[string]bool)
 	for parent := range parents {
 		moreParents = merge(moreParents, getParents(input, parent))
-		input[parent] = nil
 	}
 
 	return merge(parents, moreParents)
 }
 
-func merge(inputA, inputB map[string]bool) map[string]bool {
-	result := make(map[string]bool)
+func merge(inputA, inputB tracker) tracker {
+	result := make(tracker)
 
 	for key, value := range inputA {
 		result[key] = value
