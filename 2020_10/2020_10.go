ed57a858480087377bb3ff1a8a2fb500c297e811
soojeen <soojeen@gmail.com>
2020-12-10 09:08:09 -0500

    initial 10

---
+++ 2020_10/2020_10.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+import "log"
+import "sort"
+import "strconv"
+import "strings"
+import "advent_of_code/utils"
+
+func main() {
+	rawInput, readError := utils.ReadInput("input.txt")
+	if readError != nil {
+		log.Fatal(readError)
+	}
+
+	input, parseError := parseInput(rawInput)
+	if parseError != nil {
+		log.Fatal(parseError)
+	}
+
+	resultA := findDistribution(input)
+	// resultB := processB(input, resultA, upperIndex)
+
+	fmt.Println("a:", resultA)
+	// fmt.Println("b:", resultB)
+}
+
+func parseInput(input string) ([]int, error) {
+	var err error
+	lines := strings.Split(input, "\n")
+	numbers := make([]int, len(input))
+
+	for i, line := range lines {
+		number, error := strconv.Atoi(line)
+		if error != nil {
+			err = error
+			break
+		}
+
+		numbers[i] = number
+	}
+
+	return numbers, err
+}
+
+func findDistribution(input []int) int {
+	sort.Ints(input)
+	diffs := make(map[int]int)
+	current := 0
+
+	for _, number := range input {
+
+	}
+
+	return 0
+}
