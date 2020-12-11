f4324a0da36ef2f52dd65c83b60c23e59abcc30f
soojeen <soojeen@gmail.com>
2020-12-11 10:48:31 -0500

    wip 11

---
+++ 2020_11/2020_11.go
@@ -0,0 +1,136 @@
+package main
+
+import "fmt"
+import "log"
+import "strings"
+import "advent_of_code/utils"
+
+type grid [][]string
+type tracker struct {
+	x int
+	y int
+}
+type seatLayout struct {
+	grid    grid
+	tracker tracker
+}
+
+const empty = "L"
+const occupied = "#"
+
+func (sl *seatLayout) runRound() bool {
+	noChange := true
+
+	return noChange
+}
+
+func (sl *seatLayout) checkSeat() (string, bool) {
+	seat := sl.grid[sl.tracker.x][sl.tracker.y]
+	adjacent := sl.getAdjacent()
+
+	switch seat {
+	case empty:
+		noOccupied := hasNoOccupiedAdjacent(adjacent)
+
+		if noOccupied {
+			return occupied, false
+		}
+		return seat, true
+
+	case occupied:
+		// TODO: occupied logic
+		fallthrough
+	default:
+		return seat, true
+	}
+}
+
+func (sl *seatLayout) getAdjacent() []string {
+	x := sl.tracker.x
+	y := sl.tracker.y
+	maxX := len(sl.grid[0]) - 1
+	maxY := len(sl.grid) - 1
+
+	if x == 0 && y == 0 {
+		return []string{sl.grid[y+1][x], sl.grid[y][x+1], sl.grid[y+1][x+1]}
+	}
+
+	if x == 0 && y == maxY {
+		return []string{sl.grid[y][x+1], sl.grid[y-1][x], sl.grid[y-1][x+1]}
+	}
+
+	if x == maxX && y == maxY {
+		return []string{sl.grid[y-1][x], sl.grid[y][x-1], sl.grid[y-1][x-1]}
+	}
+
+	if x == maxX && y == 0 {
+		return []string{sl.grid[y][x-1], sl.grid[y+1][x], sl.grid[y+1][x-1]}
+	}
+
+	if x == 0 {
+		return []string{sl.grid[y-1][x], sl.grid[y+1][x], sl.grid[y][x+1], sl.grid[y-1][x+1], sl.grid[y+1][x+1]}
+	}
+
+	if x == maxX {
+		return []string{sl.grid[y-1][x], sl.grid[y+1][x], sl.grid[y][x-1], sl.grid[y-1][x-1], sl.grid[y+1][x-1]}
+	}
+
+	if y == 0 {
+		return []string{sl.grid[y][x-1], sl.grid[y][x+1], sl.grid[y+1][x], sl.grid[y+1][x-1], sl.grid[y+1][x+1]}
+	}
+
+	if y == maxY {
+		return []string{sl.grid[y][x-1], sl.grid[y][x+1], sl.grid[y-1][x], sl.grid[y-1][x-1], sl.grid[y-1][x+1]}
+	}
+
+	top := sl.grid[y-1][x-1 : x+2]
+	bottom := sl.grid[y+1][x-1 : x+2]
+
+	result := append(top, bottom...)
+
+	return append(result, sl.grid[y][x-1], sl.grid[y][x+1])
+}
+
+func main() {
+	rawInput, readError := utils.ReadInput("input.txt")
+	if readError != nil {
+		log.Fatal(readError)
+	}
+
+	input := parseInput(rawInput)
+
+	resultA := stabalise(input)
+	// resultB := findArrangements(input)
+
+	fmt.Println("a:", resultA)
+	// fmt.Println("b:", resultB)
+}
+
+func parseInput(input string) seatLayout {
+	lines := strings.Split(input, "\n")
+	grid := make(grid, len(lines))
+
+	for i, line := range lines {
+		row := strings.Split(line, "")
+		grid[i] = row
+	}
+
+	return seatLayout{grid, tracker{0, 0}}
+}
+
+func stabalise(input seatLayout) int {
+
+	fmt.Println("a: ", input.grid[0])
+
+	return 0
+}
+
+func hasNoOccupiedAdjacent(input []string) bool {
+	for _, space := range input {
+		if space == occupied {
+			return false
+		}
+	}
+
+	return true
+}
