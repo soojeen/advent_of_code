cf61743951ac01c77cd06837e682fb5e3565d143
soojeen <soojeen@gmail.com>
2020-12-11 19:57:20 -0500

    working 11a

--- 2020_11/2020_11.go
+++ 2020_11/2020_11.go
@@ -5,27 +5,43 @@ import "log"
 import "strings"
 import "advent_of_code/utils"
 
-type grid [][]string
-type tracker struct {
-	x int
-	y int
-}
+type grid map[int]map[int]rune
 type seatLayout struct {
-	grid    grid
-	tracker tracker
+	grid grid
+	x    int
+	y    int
 }
 
-const empty = "L"
-const occupied = "#"
+const empty = 'L'
+const occupied = '#'
 
 func (sl *seatLayout) runRound() bool {
-	hasChange := false
+	isDirty := false
+	newGrid := make(grid, len(sl.grid))
 
-	return hasChange
+	for y := 0; y < len(sl.grid); y++ {
+		row := sl.grid[y]
+		newRow := make(map[int]rune, len(row))
+
+		for x := 0; x < len(row); x++ {
+			sl.x = x
+			sl.y = y
+			value, isDirtySeat := sl.checkSeat()
+
+			newRow[x] = value
+			isDirty = isDirty || isDirtySeat
+		}
+
+		newGrid[y] = newRow
+	}
+
+	sl.grid = newGrid
+
+	return isDirty
 }
 
-func (sl *seatLayout) checkSeat() (string, bool) {
-	seat := sl.grid[sl.tracker.x][sl.tracker.y]
+func (sl *seatLayout) checkSeat() (rune, bool) {
+	seat := sl.grid[sl.y][sl.x]
 	adjacent := sl.getAdjacent()
 
 	switch seat {
@@ -44,50 +60,53 @@ func (sl *seatLayout) checkSeat() (string, bool) {
 
 }
 
-func (sl *seatLayout) getAdjacent() []string {
-	x := sl.tracker.x
-	y := sl.tracker.y
-	maxX := len(sl.grid[0]) - 1
-	maxY := len(sl.grid) - 1
+func (sl *seatLayout) getAdjacent() [8]rune {
+	result := [8]rune{}
 
-	if x == 0 && y == 0 {
-		return []string{sl.grid[y+1][x], sl.grid[y][x+1], sl.grid[y+1][x+1]}
+	// top
+	result[0] = sl.grid[sl.y-1][sl.x-1]
+	result[1] = sl.grid[sl.y-1][sl.x]
+	result[2] = sl.grid[sl.y-1][sl.x+1]
+	// bottom
+	result[3] = sl.grid[sl.y+1][sl.x-1]
+	result[4] = sl.grid[sl.y+1][sl.x]
+	result[5] = sl.grid[sl.y+1][sl.x+1]
+	// mid
+	result[6] = sl.grid[sl.y][sl.x-1]
+	result[7] = sl.grid[sl.y][sl.x+1]
+
+	return result
+}
+
+func (sl *seatLayout) countOccupied() int {
+	count := 0
+
+	for _, row := range sl.grid {
+		for _, char := range row {
+			if char == occupied {
+				count++
+			}
+		}
 	}
 
-	if x == 0 && y == maxY {
-		return []string{sl.grid[y][x+1], sl.grid[y-1][x], sl.grid[y-1][x+1]}
+	return count
+}
+
+func (sl *seatLayout) prettyPrint() string {
+	prettyGrid := ""
+
+	for y := 0; y < len(sl.grid); y++ {
+		row := sl.grid[y]
+		prettyRow := ""
+
+		for x := 0; x < len(row); x++ {
+			prettyRow = prettyRow + string(row[x])
+		}
+
+		prettyGrid = prettyGrid + prettyRow + "\n"
 	}
 
-	if x == maxX && y == maxY {
-		return []string{sl.grid[y-1][x], sl.grid[y][x-1], sl.grid[y-1][x-1]}
-	}
-
-	if x == maxX && y == 0 {
-		return []string{sl.grid[y][x-1], sl.grid[y+1][x], sl.grid[y+1][x-1]}
-	}
-
-	if x == 0 {
-		return []string{sl.grid[y-1][x], sl.grid[y+1][x], sl.grid[y][x+1], sl.grid[y-1][x+1], sl.grid[y+1][x+1]}
-	}
-
-	if x == maxX {
-		return []string{sl.grid[y-1][x], sl.grid[y+1][x], sl.grid[y][x-1], sl.grid[y-1][x-1], sl.grid[y+1][x-1]}
-	}
-
-	if y == 0 {
-		return []string{sl.grid[y][x-1], sl.grid[y][x+1], sl.grid[y+1][x], sl.grid[y+1][x-1], sl.grid[y+1][x+1]}
-	}
-
-	if y == maxY {
-		return []string{sl.grid[y][x-1], sl.grid[y][x+1], sl.grid[y-1][x], sl.grid[y-1][x-1], sl.grid[y-1][x+1]}
-	}
-
-	top := sl.grid[y-1][x-1 : x+2]
-	bottom := sl.grid[y+1][x-1 : x+2]
-
-	result := append(top, bottom...)
-
-	return append(result, sl.grid[y][x-1], sl.grid[y][x+1])
+	return prettyGrid
 }
 
 func main() {
@@ -109,22 +128,30 @@ func parseInput(input string) seatLayout {
 	lines := strings.Split(input, "\n")
 	grid := make(grid, len(lines))
 
-	for i, line := range lines {
-		row := strings.Split(line, "")
-		grid[i] = row
+	for y, line := range lines {
+		row := make(map[int]rune)
+
+		for x, char := range line {
+			row[x] = char
+		}
+
+		grid[y] = row
 	}
 
-	return seatLayout{grid, tracker{0, 0}}
+	return seatLayout{grid, 0, 0}
 }
 
 func stabalise(input seatLayout) int {
+	for {
+		isDirty := input.runRound()
 
-	fmt.Println("a: ", input.grid[0])
-
-	return 0
+		if !isDirty {
+			return input.countOccupied()
+		}
+	}
 }
 
-func hasNoOccupiedAdjacent(input []string) bool {
+func hasNoOccupiedAdjacent(input [8]rune) bool {
 	for _, space := range input {
 		if space == occupied {
 			return false
@@ -134,7 +161,7 @@ func hasNoOccupiedAdjacent(input []string) bool {
 	return true
 }
 
-func hasOccupiedAdjacent(input []string) bool {
+func hasOccupiedAdjacent(input [8]rune) bool {
 	count := 0
 
 	for _, space := range input {
